ad4f81d2ef324d97bdadbef740f6857ed4ccb17c
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2025-03-09 18:49:37 +0530

    hero section completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './Home.css'
 import  Navbar  from "../../components/Navbar/Navbar";
+import hero from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
 
 function Home(){
   return (
     <>
       <Navbar />
+      <div className="hero">
+        <img src={hero} alt="" className="hero-banner" />
+        <div className='hero-caption'>
+          <img src={hero_title} alt="" className="caption-img" />
+          <p className='hero-description'>
+            Discovering his ties to a secret ancient order, a young man living in
+            modern Istanbul embarks on a quest to save the city from an enemy.
+          </p>
+          <div className='hero-btns'>
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark'><img src={info_icon} alt="" />More Info</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
