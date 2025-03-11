7d9b155b26069af1e5ac70846a51aeafa9282fb7
arunbchandran100 <121543517+arunbchandran100@users.noreply.github.com>
2025-03-11 14:23:58 +0530

    completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
-import "./NavBar.css";
+import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import search_icon from "../../assets/search_icon.svg";
 import bell_icon from "../../assets/bell_icon.svg";
 import profile_img from "../../assets/profile_img.png";
 import caret_icon from "../../assets/caret_icon.svg";
 import { logout } from "../../firebase";
-// import { logout } from "../../firebase";
 
 function Navbar(){
     const navRef = useRef();
